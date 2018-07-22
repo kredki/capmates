36162e55209017901f095a15251f4fa461e22a13
TL <31960565+kredki@users.noreply.github.com>
2018-07-22 18:12:07 +0200

    init

---
+++ src/main/java/com/capgemini/jstk/capmates/CapmatesApplication.java
@@ -0,0 +1,12 @@
+package com.capgemini.jstk.capmates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapmatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapmatesApplication.class, args);
+	}
+}
